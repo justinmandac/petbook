9c8461e06d8438bb830e2843c5883e78724598eb
justinmandac <justinmandac@gmail.com>
2017-08-26 16:22:28 +0800

    Generate index.html on build via html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const config = {
   entry : path.resolve(__dirname, './web/src/main.js'),
   output: {
     filename: 'app.bundle.js',
-    path: path(__dirname, 'wwww'),
+    path: path.resolve(__dirname, 'www'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './web/src/index.html',
+    }),
+  ]
 };
 
 module.exports = config;
