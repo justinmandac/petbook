30454d67648aa3490b5a3490d101503115870aee
justinmandac <justinmandac@gmail.com>
2017-08-26 17:06:31 +0800

    Enabled .jsx loading

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,23 @@ const config = {
     filename: 'app.bundle.js',
     path: path.resolve(__dirname, 'www'),
   },
+  module: {
+    loaders: [
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader',
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './web/src/index.html',
     }),
-  ]
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.json', '.html', '.css'],
+  },
 };
 
 module.exports = config;
