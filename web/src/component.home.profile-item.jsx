1e2878b86edf0249945d546c69c0cb75d1016d43
justinmandac <justinmandac@gmail.com>
2017-08-27 21:54:42 +0800

    Update

--- web/src/component.home.profile-item.jsx
+++ web/src/component.home.profile-item.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 export default function ProfileItem({
   id,
   name,
-  typebreed,
-  onClick = () => {}}) {
+  typebreed
+}) {
   
     return (
       <Link to={{
