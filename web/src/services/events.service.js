b6159ebaad1b14299f540807df88bb942df35db4
justinmandac <justinmandac@gmail.com>
2017-08-27 22:31:16 +0800

    Added mock events

--- web/src/services/events.service.js
+++ web/src/services/events.service.js
@@ -2,11 +2,33 @@
  * @fileoverview Events Service
 */
 
+const mockPetEvents = [
+  {
+    petId: 1,
+    title: '5-in-1 Vaccination',
+    location: 'Paraan Veterinary Clinic, Antipolo',
+    datetime: '2017-09-14',
+    type: 'Vaccination',
+    notes: 'n/a',
+    frequency: '90'
+  },
+  {
+    petId: 1,
+    title: 'Bath',
+    location: 'Home',
+    datetime: '2017-09-01T11:00:00',
+    type: 'Bath',
+    notes: 'n/a',
+    frequency: '14',
+  },
+];
+
+
 /** @constructor */
 export function PetEventListItem() {};
 
 /** @type {(!Date|string)} */
-PetEventListItem.prototype.date = null;
+PetEventListItem.prototype.datetime = null;
 
 /** @type {string} */
 PetEventListItem.prototype.title = '';
@@ -62,5 +84,9 @@ export function GetPetEvents(
   startDate,
   endDate,
   order = ORDER_VALUES.DESC) {
-
+  return new Promise((resolve, reject) => {
+    const filteredEvents = 
+      mockPetEvents.filter(({petId :id}) => id == petId);
+    resolve(filteredEvents);
+  });
 }
