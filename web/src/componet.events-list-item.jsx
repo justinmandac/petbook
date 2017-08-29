7af1ee5e79a8f0042344eb71f5905404a2e8cd5b
Bien Justin Mandac <justinmandac@gmail.com>
2017-08-30 05:20:03 +0800

    Created scaffolding for Event-related items for the home page

---
+++ web/src/componet.events-list-item.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function EventsListItem() {
+    return(
+    <li className="events-list-item">
+
+    </li>);
+}
---
+++ web/src/componet.events-list.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EventsList() {
+    return (<ul className="events-list"></ul>);
+}
