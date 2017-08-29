8fd61fd21ed6a0c1db418134d364a9f287a50964
Bien Justin Mandac <justinmandac@gmail.com>
2017-08-30 05:02:24 +0800

    Created the component for the Manage Pets Page. State will be stored from this component hence all child component are expected to be pure components. All Profile-related CRUD operations will be performed from this page. Created a separate but similar component for Profile Items.

---
+++ web/src/component.profile-list-item.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const ProfileItemStyles = {
+  display: 'flex',
+  height: '72px',
+  width: '100%',
+};
+
+export default function ProfileListItem({
+  id,
+  name,
+  typebreed
+}) {
+  
+    return (
+      <Link to={{
+        pathname :"/profile/" + id
+      }} replace>
+        <div className="profile-item" style={ProfileItemStyles}>
+          <div className="profile-item__info">
+          {name}, {typebreed}
+          </div>
+        </div>
+      </Link>
+  );
+}
--- web/src/page.manage-pets.jsx
+++ web/src/page.manage-pets.jsx
@@ -1,15 +1,48 @@
 import React, {Component} from 'react';
+import {GetProfiles} from './services/profiles.service';
+import ProfileListItem from './component.profile-list-item';
 
 export default class ManagePetsPage extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      profiles: [],
+      page: 0,
+    };
+  }
+
+  componentDidMount() {
+    console.log('Loading[ManagePage]');
+    const {userId} = this.props;
+
+    GetProfiles(userId).then((profiles) => {
+      this.setState((prevState, props) => {
+        return Object.assign(prevState, { profiles });
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    // Restore to initial state.
+    this.setState((prevState, props) => Object.assign(prevState, {
+      profiles: [],
+      page: 0,
+    }));
   }
 
   render() {
     const {userId} = this.props;
+    const {profiles} = this.state;
+    const profilesList = profiles.map((profile) => {
+      return (<ProfileListItem 
+                key={profile.id}
+                id={profile.id}
+                name={profile.name} />);
+    });
     return (
       <section id="managePetsPage" className="page manage-page">
         My Pets
+        {profilesList}
       </section>
     );
   }
